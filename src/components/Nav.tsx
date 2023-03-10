0a59a50b0ed99826024fa05c7fbe58c6727406e4
NiiXu <chenhengxv@outlook.com>
2023-03-10 15:56:21 +0800

    修复Nav点击

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,18 +6,24 @@ import Icon from './icon';
 
 const NavWrapper = styled.nav`
   line-height: 24px;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+
   > ul {
-    display:flex;
-    > li{
+    display: flex;
+
+    > li {
+      >a{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding: 4px 0;
+
+      }
       width: 33.3333%;
-      text-align:center;
-      display: flex;
-      padding: 4px 0;
-      flex-direction:column ;
-      justify-content: center;
-      align-items: center;
-      .icon{
+      text-align: center;
+      
+      .icon {
         width: 24px;
         height: 24px;
       }
@@ -26,20 +32,26 @@ const NavWrapper = styled.nav`
 `;
 
 const Nav = () => {
-    return(
+    return (
         <NavWrapper>
             <ul>
                 <li>
-                    <Icon name={'tag'}/>
-                    <Link to="/tags">标签页</Link>
+                    <Link to="/tags">
+                        <Icon name={'tag'}/>
+                        标签页
+                    </Link>
                 </li>
                 <li>
-                    <Icon name={'money'}/>
-                    <Link to="/money">记账页</Link>
+                    <Link to="/money">
+                        <Icon name={'money'}/>
+                        记账页
+                    </Link>
                 </li>
                 <li>
-                    <Icon name={'chart'}/>
-                    <Link to="/statistics">统计页</Link>
+                    <Link to="/statistics">
+                        <Icon name={'chart'}/>
+                        统计页
+                    </Link>
                 </li>
             </ul>
         </NavWrapper>
