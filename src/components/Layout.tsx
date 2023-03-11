071efcef22d90e8c79124c23c15830f056be6a40
NiiXu <chenhengxv@outlook.com>
2023-03-11 23:53:39 +0800

    不需要的

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Nav from './Nav';
+import styled from 'styled-components';
+
+// type Props ={
+//     children:string
+// }
+const Wrapper = styled.div`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+const Main = styled.div`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Layout = (props:any)=>{
+    return(
+        <Wrapper>
+            <Main className={props.className}>
+                {props.children}
+            </Main>
+            <Nav/>
+        </Wrapper>
+    )
+}
+
+export default Layout
